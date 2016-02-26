fd3efb203ca1aeb99e85d5090293165642d705ad
Fabio Ferreira <fabio.ferreira@controlid.com.br>
2016-02-26 15:56:02 -0300

    Captura de Imagem do Leitor Futronic

--- test-CS-Futronic/MainWindow.xaml.cs
+++ test-CS-Futronic/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Drawing;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TestFutronic
 {
@@ -19,9 +11,78 @@ namespace TestFutronic
     /// </summary>
     public partial class MainWindow : Window
     {
+        Futronic leitor; // contrala a API leitor Frutronic
+        DispatcherTimer timer; // timer que busca novos eventos a cada 1 segundo
+        int dedo; // contado da posição do dedo
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // meus componentes
+            leitor = new Futronic();
+            timer = new DispatcherTimer();
         }
+
+        private void Window_Load(object sender, RoutedEventArgs e)
+        {
+            dedo = 1;
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            timer.Stop();
+            if (leitor.Connected)
+                leitor.Dispose();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (leitor.Connected)
+                {
+                    if (leitor.IsFinger())
+                    {
+                        txtStatus.Text = "Dedo " + dedo;
+                        using (Bitmap bmp = leitor.ExportBitMap())
+                        {
+                            switch (dedo)
+                            {
+                                case 1:
+                                    dedo1.Source = bmp.ToBitmapSource();
+                                    break;
+                                case 2:
+                                    dedo2.Source = bmp.ToBitmapSource();
+                                    break;
+                                case 3:
+                                    dedo3.Source = bmp.ToBitmapSource();
+                                    break;
+                            }
+                            dedo++;
+                            if (dedo > 3)
+                                dedo = 1;
+                        }
+                    }
+                    else
+                        txtStatus.Text = "Coloque o dedo para a imagem " + dedo;
+                }
+                else
+                {
+                    if (leitor.Init())
+                        txtStatus.Text = "Leitor Futronic FS-80 reconhecido";
+                    else
+                        txtStatus.Text = "Conecte o leitor Futronic FS-80 !";
+                }
+            }
+            catch (Exception ex)
+            {
+                txtStatus.Text = ex.Message;
+            }
+        }
+        
     }
 }
